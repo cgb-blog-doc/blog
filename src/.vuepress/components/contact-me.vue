f85f33f22cfb0728774a0d34e5695eea0eddcf21
JasonBai007 <byygkcg@163.com>
2022-03-20 11:26:39 +0800

    添加动画效果

--- src/.vuepress/components/contact-me.vue
+++ src/.vuepress/components/contact-me.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="wrap">
+    <!-- 微信 -->
     <div class="item">
       <div class="left">
         <img src="/blog/contact/wechat.png" alt />
@@ -9,6 +10,7 @@
         <p>1105843831</p>
       </div>
     </div>
+    <!-- 邮箱 -->
     <div class="item">
       <div class="left">
         <img src="/blog/contact/email.png" alt />
@@ -20,6 +22,7 @@
         </p>
       </div>
     </div>
+    <!-- 地址 -->
     <div class="item">
       <div class="left">
         <img src="/blog/contact/address.png" alt />
@@ -42,12 +45,37 @@ export default {
 .wrap {
   margin: 50px 0;
 }
+
 .item {
   display: flex;
   align-items: center;
   justify-content: space-between;
   margin-bottom: 20px;
+  opacity: 0; /* 开头隐藏 */
 }
+
+/* forwards表示：动画结束后，元素将保持动画的最后一帧状态 */
+.item:nth-of-type(1) {
+  animation: fadeIn 0.8s 0s forwards;
+}
+.item:nth-of-type(2) {
+  animation: fadeIn 0.8s 0.4s forwards;
+}
+.item:nth-of-type(3) {
+  animation: fadeIn 0.8s 0.6s forwards;
+}
+
+@keyframes fadeIn {
+  0% {
+    opacity: 0;
+    transform: translateY(20px);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(0px);
+  }
+}
+
 .left {
   flex-shrink: 0;
   width: 80px;
@@ -65,27 +93,30 @@ export default {
 .item:nth-of-type(3) .left {
   background: #fedfd7;
 }
+
 .left img {
   width: 30px;
   height: 30px;
   display: block;
   margin: auto;
 }
+
 .right {
   flex-grow: 1;
 }
+
 h2 {
   margin-top: 15px;
   margin-bottom: 0;
   border: none;
 }
+
 p {
   margin-top: 5px;
 }
 p a:hover {
   cursor: pointer;
 }
-
 p::selection {
   color: #fff;
   background: #46bd87;
